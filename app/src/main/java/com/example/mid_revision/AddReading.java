a8a811b07961aa1d1e1f8cebc49e352381f3d52d
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-08-23 19:32:03 +0530

    null values handled

--- app/src/main/java/com/example/mid_revision/AddReading.java
+++ app/src/main/java/com/example/mid_revision/AddReading.java
@@ -9,27 +9,25 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddReading extends AppCompatActivity {
-    Button viewBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reading);
 
         changeFragments();
-
-        viewBtn = findViewById(R.id.viewBtn);
     }
 
     public void changeFragments() {
         TextView author, title;
         Intent intent = getIntent();
+        Fragment fragment1, fragment2;
         String status = intent.getStringExtra("READING_TYPE");
+
         author = findViewById(R.id.lblAuthor);
         title = findViewById(R.id.lblTitle);
 
@@ -38,17 +36,16 @@ public class AddReading extends AppCompatActivity {
                 author.setText("Author");
                 title.setText("Book Titile");
 
-                Fragment fragment1;
                 fragment1 = new BookFragment();
                 FragmentManager fm = getSupportFragmentManager();
                 FragmentTransaction ft = fm.beginTransaction();
                 ft.replace(R.id.fragmentDefault,fragment1);
                 ft.commit();
-            } else if(status.equals("0")) {
+            }
+            if(status.equals("0")) {
                 author.setText("Editor");
                 title.setText("Title of the article");
 
-                Fragment fragment2;
                 fragment2 = new PaperFragment();
                 FragmentManager fm = getSupportFragmentManager();
                 FragmentTransaction ft = fm.beginTransaction();
@@ -65,17 +62,21 @@ public class AddReading extends AppCompatActivity {
         author = findViewById(R.id.txtAuthor);
         title = findViewById(R.id.txtTitle);
 
-        String authorVal = author.getText().toString();
-        String titleVal = title.getText().toString();
+        if(author.length() >= 1 || title.length() >= 1) {
+            String authorVal = author.getText().toString();
+            String titleVal = title.getText().toString();
 
-        System.out.println(authorVal);
-        System.out.println(titleVal);
+            System.out.println(authorVal);
+            System.out.println(titleVal);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(AddReading.this);
-        builder.setTitle("You're Reading");
-        builder.setMessage(titleVal + " by " + authorVal);
-        builder.setPositiveButton("OK", null);
-        dialog = builder.create();
-        dialog.show();
+            AlertDialog.Builder builder = new AlertDialog.Builder(AddReading.this);
+            builder.setTitle("You're Reading");
+            builder.setMessage(titleVal + " by " + authorVal);
+            builder.setPositiveButton("OK", null);
+            dialog = builder.create();
+            dialog.show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Reader's name cannot be empty !", Toast.LENGTH_LONG).show();
+        }
     }
 }
